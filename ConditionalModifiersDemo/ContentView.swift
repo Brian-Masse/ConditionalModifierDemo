ac221a0ff1debdbed1b59f743a40481117e689bf
Brian Masse <brianm25it@gmail.com>
2023-12-29 11:29:56 -0500

    added preview code

--- ConditionalModifiersDemo/ContentView.swift
+++ ConditionalModifiersDemo/ContentView.swift
@@ -24,11 +24,13 @@ struct ContentView: View {
     
     @ViewBuilder
     private func makeToggle() -> some View {
-        
-        Text( "toggle" )
-            .padding()
+        HStack {
+            Text( "toggle" )
+            Image(systemName: "arrow.right")
+        }
+            .padding(10)
             .background( toggle ? .red : .gray.opacity(0.5) )
-            .cornerRadius(20)
+            .cornerRadius(15)
             .onTapGesture { withAnimation {
                 toggle.toggle()
             } }
@@ -36,20 +38,27 @@ struct ContentView: View {
     
 //    MARK: Body
     var body: some View {
-        makeToggle()
-            .padding(.bottom)
-        
-        Text("hello World!")
+        VStack {
+            makeToggle()
+                .padding(.bottom)
+            
+            HStack {
+                Text("hello World!")
+                Image(systemName: "globe.americas")
+            }
             .padding(7)
             .if(!toggle) { view in
                 view
                     .padding(7)
-                    .background(.red)
+                    .background(Colors.lightAccentGreen)
                     .cornerRadius(20)
                     .bold(true)
                     .font(.title)
             }
             .if(toggle) { view in view.opacity(0.5) }
+        }.padding(.vertical)
+        
+        Spacer()
     }
 }
 
@@ -57,4 +66,23 @@ struct ContentView: View {
     ContentView()
 }
 
+//MARK: Colors
+class Colors {
+    static let lightGrey = makeColor(255, 255, 255)
+    static let secondaryLightColor = makeColor( 235, 235, 235 )
+    static let darkGrey = Color(red: 0.1, green: 0.1, blue: 0.1).opacity(0.9)
+    static let accentGreen = makeColor(95, 255, 135)
+    static let lightAccentGreen = makeColor(66, 122, 69)
+    
+    static let yellow = makeColor(234, 169, 40)
+    static let pink = makeColor(198, 62, 120)
+    static let purple = makeColor(106, 38, 153)
+    static let grape = makeColor(70, 42, 171)
+    static let blue = makeColor(69, 121, 251)
+    static let red = makeColor(236, 81, 46)
+    
+    private static func makeColor( _ r: CGFloat, _ g: CGFloat, _ b: CGFloat ) -> Color {
+        Color(red: r / 255, green: g / 255, blue: b / 255)
+    }
+}
 
